37b68fff45ccf372bc6eaf286899ba49fb21b7c7
ManishaTulsiyani <89012436+ManishaTulsiyani@users.noreply.github.com>
2021-08-18 00:20:32 +0530

    Seventh Commit

--- TDD_Kata1_StringCalculator/src/StringCalculator.java
+++ TDD_Kata1_StringCalculator/src/StringCalculator.java
@@ -1,10 +1,19 @@
 
 public class StringCalculator {
+	
 	public int Add(String numbers) {
+		String[] nos=numbers.split(",");
+		
 		if(isEmpty(numbers)) {
 			return 0;
 		}
-		return stringToInt(numbers);
+		if(numbers.length()==1) {
+			return stringToInt(numbers);
+		}
+		else {
+			return Integer.parseInt(nos[0])+Integer.parseInt(nos[1]);
+			
+		}
 	}
 	
 	private boolean isEmpty(String numbers) {
