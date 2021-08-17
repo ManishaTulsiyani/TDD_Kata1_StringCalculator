1829f263076f6d9b616952b5f8c28a0145b2ee41
ManishaTulsiyani <89012436+ManishaTulsiyani@users.noreply.github.com>
2021-08-17 23:52:00 +0530

    Third Commit

--- TDD_Kata1_StringCalculator/src/TestCalculate.java
+++ TDD_Kata1_StringCalculator/src/TestCalculate.java
@@ -1,12 +1,23 @@
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import static org.testng.Assert.assertEquals;
 
 @Test
 public class TestCalculate {
 	
+    private StringCalculator sc;
+	
+	@BeforeTest
+	public void init() {
+		sc=new StringCalculator();
+	}
+	
 	public void emptyStringReturnsZero(){
-		StringCalculator sc=new StringCalculator();
 		assertEquals(sc.Add(""),0);
+	}
+	
+	public void singleNumberReturnsValue() {
+		assertEquals(sc.Add("1"),1);
 	}
  
 }
